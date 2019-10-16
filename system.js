5069985a7d30dd603382958d50a983df025920e7
FrancoAdN <francoadinapoli@gmail.com>
2019-10-16 10:10:01 -0300

    add

--- system.js
+++ system.js
@@ -38,17 +38,17 @@ app.get('/ord_nac', (req, resp) => {
               resp.send("0");
           }
           //PARSING FROM RESULTS TO TABLE
-          let index = result[0].id_nac;
-          let json = {id: result[0].id_nac, emp: result[0].emp, inco: result[0].fdp, mon: result[0].moneda,
+          let index = result[0].id_ext;
+          let json = {id: result[0].id_ext, emp: result[0].emp, inco: result[0].incoterm, mon: result[0].moneda,
             pmde: result[0].pmde, fecha: result[0].fecha, orden: result[0].orden, tabla:[{cant: result[0].cant, descr: result[0].descr, punit: result[0].punit}]};
           let arr = []
           for(let i = 1; i < result.length; i++){
-              if(result[i].id_nac == index)
+              if(result[i].id_ext == index)
                 json.tabla.push({cant: result[i].cant, descr: result[i].descr, punit: result[i].punit});
               else{
                   arr.push(json);
-                  index = result[i].id_nac;
-                  json = {id: result[i].id_nac, emp: result[i].emp, inco: result[i].fdp, mon: result[i].moneda,
+                  index = result[i].id_ext;
+                  json = {id: result[i].id_ext, emp: result[i].emp, inco: result[i].incoterm, mon: result[i].moneda,
                     pmde: result[i].pmde, fecha: result[i].fecha, orden: result[i].orden, tabla:[{cant: result[i].cant, descr: result[i].descr, punit: result[i].punit}]};
               }
               if(i == result.length -1)
