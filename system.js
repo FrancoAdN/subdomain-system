fa3abe7ef788120eba87044199680fd1ec79b478
FrancoAdN <francoadinapoli@gmail.com>
2019-11-01 10:25:51 -0300

    add

--- system.js
+++ system.js
@@ -1305,7 +1305,7 @@ app.post('/sol', (req, resp) => {
     const con = connectionSQL();
     const data = req.body;
     console.log(data);
-    console.log(data.orden);
+    console.log(data.cliente);
     const nof = parseInt(data.orden.split('-')[1]);
     let sql =  `INSERT INTO solicitudes (orden, fecha, procedencia, cliente, descr) values ('${data.orden}', '${data.fecha}', '${data.proc}', '${data.dias}', '${data.cliente}', '${data.descr}');
     UPDATE last SET num = ${nof};`;
