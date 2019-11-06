34f8db1d65265b2ad8612184b80066ba73a1b662
tomasvera1 <tomasveratec@gmail.com>
2019-11-06 11:49:24 -0300

    add

--- sys/MaxPowerSystem/compNacUC.Designer.cs
+++ sys/MaxPowerSystem/compNacUC.Designer.cs
@@ -52,6 +52,7 @@
             this.label8 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.BoxRefOrd = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
@@ -86,7 +87,7 @@
             this.tableLayoutPanel1.ColumnCount = 3;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.90278F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 69.09722F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 123F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 124F));
             this.tableLayoutPanel1.Controls.Add(this.DescBox, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.CantBox, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.UnitPrBox, 1, 1);
@@ -109,7 +110,7 @@
             this.DescBox.Location = new System.Drawing.Point(75, 51);
             this.DescBox.Multiline = true;
             this.DescBox.Name = "DescBox";
-            this.DescBox.Size = new System.Drawing.Size(157, 81);
+            this.DescBox.Size = new System.Drawing.Size(156, 81);
             this.DescBox.TabIndex = 34;
             // 
             // CantBox
@@ -128,10 +129,10 @@
             // 
             this.UnitPrBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.UnitPrBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.UnitPrBox.Location = new System.Drawing.Point(238, 51);
+            this.UnitPrBox.Location = new System.Drawing.Point(237, 51);
             this.UnitPrBox.Multiline = true;
             this.UnitPrBox.Name = "UnitPrBox";
-            this.UnitPrBox.Size = new System.Drawing.Size(118, 81);
+            this.UnitPrBox.Size = new System.Drawing.Size(119, 81);
             this.UnitPrBox.TabIndex = 35;
             this.UnitPrBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.UnitPrBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.notNumberCheck);
@@ -155,7 +156,7 @@
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
             this.label4.Location = new System.Drawing.Point(75, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(157, 48);
+            this.label4.Size = new System.Drawing.Size(156, 48);
             this.label4.TabIndex = 36;
             this.label4.Text = "Descripción";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -165,9 +166,9 @@
             this.label6.AutoSize = true;
             this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
-            this.label6.Location = new System.Drawing.Point(238, 0);
+            this.label6.Location = new System.Drawing.Point(237, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(118, 48);
+            this.label6.Size = new System.Drawing.Size(119, 48);
             this.label6.TabIndex = 37;
             this.label6.Text = "Precio Unitario";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -317,10 +318,18 @@
             this.label2.TabIndex = 116;
             this.label2.Text = "Orden de compra Nacional";
             // 
+            // BoxRefOrd
+            // 
+            this.BoxRefOrd.Location = new System.Drawing.Point(321, 273);
+            this.BoxRefOrd.Name = "BoxRefOrd";
+            this.BoxRefOrd.Size = new System.Drawing.Size(100, 20);
+            this.BoxRefOrd.TabIndex = 119;
+            // 
             // compNacUC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.BoxRefOrd);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox2);
@@ -375,5 +384,6 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox BoxRefOrd;
     }
 }
